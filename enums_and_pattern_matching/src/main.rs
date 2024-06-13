748a516552abf26ff09793cc883e55908a236e7f
Bhavik-punmiya <Bhavikpunmiya@gmail.com>
2024-06-13 16:46:43 +0530

    rust enums

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+}
+
+struct IpAddrKind {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum Message{
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+
+
+impl Message{
+    fn some_function (){
+        println!("Let's Get Rusty");
+    }
+}
+
+fn main(){
+    let localhost : IpAddrKind = IpAddrKind {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1");
+    };
+
+    enum Option<T> {
+        Some(T), 
+        None,
+    }
+
+
+
+}
+
+fn route(ip_kind: IpAddrKind){
+
 }
