c5da7a248ec072af5a646c315808379a20adf753
Bhavik-punmiya <Bhavikpunmiya@gmail.com>
2024-06-13 14:02:52 +0530

    Ownership and references completed

--- ownership_in_rust/src/main.rs
+++ ownership_in_rust/src/main.rs
@@ -1,7 +1,20 @@
 fn main(){
-
+    let mut s = String::from("Hello, World!");
+    let hello = &s[0..5];
+    let _world = &s[7..12];
+    let _s2 = "Hello Rust";
+    let word: &str= first_word(&_s2);
+    println!("The value of hello is: {}", hello);
 }
 
-fn dangle(){
-    
+
+fn first_word(s: &String)-> &str{
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[0..i];
+        }
+    }
+    &s[..]
+
 }
