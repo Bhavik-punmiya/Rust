826d3104dca707c15c74357846c12a4f0ce6e7e6
Bhavik-punmiya <Bhavikpunmiya@gmail.com>
2024-06-13 15:11:14 +0530

    struct update

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,7 +6,30 @@ struct Rectangle{
 
 impl Rectangle {
     fn area(&self) -> u32 {
-        self.width * self.height
+        self.width * struct Rectange{
+            width: u32,
+            height: u32,
+        }
+        
+        
+        fn main(){
+            let rec = Rectange{
+                width: 30,
+                height: 50,
+            };
+        
+            println!(
+                "The area of the rectangle is {} square pixels.",
+                area(&rec)
+            );
+        
+            
+        
+        }
+        
+        fn area(rect: &Rectange) -> u32 {
+            rect.width * rect.height
+        }   self.height
     }
 
     fn can_hold(&self, other: Rectangle) -> bool{
